b14daec240088c2663340d9651e2ac71e66c3c6c
Joseph Buchma <josephbuchma@gmail.com>
2017-06-26 23:02:17 +0300

    benchmarks

--- ordsync_test.go
+++ ordsync_test.go
@@ -1,10 +1,62 @@
 package ordsync
 
 import (
+	"math/rand"
 	"reflect"
 	"testing"
 	"time"
 )
+
+func genRandomDurations(n, rng int) []time.Duration {
+	ret := make([]time.Duration, n)
+	for i := 0; i < n; i++ {
+		ret[i] = time.Duration(rand.Intn(rng) + 1)
+	}
+	return ret
+}
+
+var benchJobs = genRandomDurations(1000, 20)
+
+func BenchmarkDeferGroup(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		jobs := benchJobs
+		results := []time.Duration{}
+
+		dg := DeferGroup{}
+		for _, j := range jobs {
+			j := j
+			deferred := dg.Defer()
+			go func() {
+				time.Sleep(j * time.Millisecond)
+				deferred.Do(func() {
+					results = append(results, j) // no data race here
+				})
+			}()
+		}
+
+		dg.Wait() // Wait until last deferred function is done
+	}
+}
+
+func BenchmarkFanOut(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		jobs := benchJobs
+		results := []time.Duration{}
+
+		fanIn := make(chan time.Duration)
+		for _, j := range jobs {
+			j := j
+			go func() {
+				time.Sleep(j * time.Millisecond)
+				fanIn <- j
+			}()
+		}
+
+		for _, _ = range jobs {
+			results = append(results, <-fanIn)
+		}
+	}
+}
 
 func TestDeferGroup(t *testing.T) {
 	for i := 0; i < 20; i++ {
